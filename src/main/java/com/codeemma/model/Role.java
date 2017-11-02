db2ce630044d0aca9ad04b17fa03c6407a825a29
Emmanuel Olayinka <eakinolayinka@gmail.com>
2017-11-02 08:26:39 +0100

    add getters and setters

--- src/main/java/com/codeemma/model/Role.java
+++ src/main/java/com/codeemma/model/Role.java
@@ -27,4 +27,6 @@ public class Role {
         this.user = user;
         this.roleType = roleType;
     }
+
+
 }
--- src/main/java/com/codeemma/model/User.java
+++ src/main/java/com/codeemma/model/User.java
@@ -1,14 +1,17 @@
 package com.codeemma.model;
 
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Set;
+import java.util.List;
 
 /**
  * Created by emmanuel on 10/27/17.
@@ -23,11 +26,11 @@ public class User implements UserDetails, Serializable {
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
 
-    @Column(name = "firstName")
-    private String firstName;
+    @Column(name = "firstname")
+    private String firstname;
 
-    @Column(name = "surname")
-    private String surname;
+    @Column(name = "lastname")
+    private String lastname;
 
     @Column(name = "email", nullable = false, unique = true)
     private String email;
@@ -44,27 +47,98 @@ public class User implements UserDetails, Serializable {
 //    private Address address;
     @Temporal(TemporalType.DATE)
     @Column(name = "dateCreated")
+    @CreationTimestamp
     private Date dateCreated;
 
-//    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinColumn(name = "role_id")
-//    private Role role;
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "role_id")
+    private Role role;
 
 
     public User() {
     }
 
-    public User(String firstName, String surname, String email, String username, String password, String sex) {
-        this.firstName = firstName;
-        this.surname = surname;
+    public User(String firstname, String lastname, String email, String username, String password, String sex) {
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.email = email;
         this.username = username;
         this.password = password;
         this.sex = sex;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//        if  (role != null)
+//            String roleName = "ROLE_"+
+//        authorities.add(new SimpleGrantedAuthority(roleName));
+//        }
+//        return authorities;
         return null;
     }
 
@@ -75,7 +149,7 @@ public class User implements UserDetails, Serializable {
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
